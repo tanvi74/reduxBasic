880452c476a6e00cd0d1167be3dbd8db3b8c7a57
maps <1998tanvi@gmail.com>
2020-09-13 16:40:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './App.css';
-
 import FrozenDept from './components/FrozenDept';
 import MeatDept from './components/MeatDept';
 import ProduceDept from './components/ProduceDept';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Main from './components/Main'
 
 function App() {
   return (
-    <div className="App">
-      <h2>Frozen Department</h2>
-      <FrozenDept />
-      <h2>Meat Department</h2>
-      <MeatDept />
-      <h2>Producer Department</h2>
-      <ProduceDept />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" component={NavBar} />
+        <Route path="/main" component={Main} />
+        <Route path="/frozen-dept" component={FrozenDept} />
+        <Route path="/meat-dept" component={MeatDept} />
+        <Route path="/produce-dept" component={ProduceDept} />
+      </div>
+    </Router>
   );
 }
 
